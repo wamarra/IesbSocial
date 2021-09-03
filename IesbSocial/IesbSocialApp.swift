281e33cef79d8a603868f415b589bd5319c9bd04
Wesley Marra <cast.wesleya.utic@sebrae.com.br>
2021-09-03 08:24:35 -0300

    Initial Commit

---
+++ IesbSocial/IesbSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  IesbSocialApp.swift
+//  IesbSocial
+//
+//  Created by Wesley Marra on 03/09/21.
+//
+
+import SwiftUI
+
+@main
+struct IesbSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
